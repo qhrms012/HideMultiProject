fb3c7d11f54db8e506062ee55d937d1e2ddb9222
boguen <qhrms012@naver.com>
2024-11-25 21:00:45 +0900

    [add] Runstate추가

---
+++ Assets/01.Scripts/State/RunState.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class RunState : Istate
+{
+    private StateMachine stateMachine;
+    private Animator animator;
+
+    public RunState(StateMachine machine, Animator animator)
+    {
+        stateMachine = machine;
+        this.animator = animator;
+    }
+
+    public void Enter()
+    {
+        animator.Play("Run");
+    }
+
+    public void Execute(Vector2 playerVector)
+    {
+        if(playerVector.magnitude == 0) 
+        {
+            stateMachine.SetState(new IdleState(stateMachine,animator));
+        }
+    }
+
+    public void Exit()
+    {
+        Debug.Log("뛰는상태 종료");
+    }
+}
