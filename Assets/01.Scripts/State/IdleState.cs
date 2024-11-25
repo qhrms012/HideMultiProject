9bc656dbb0c3e57545815d7c3f83d081a22f17d8
boguen <qhrms012@naver.com>
2024-11-25 20:57:57 +0900

    [add] idleState 추가

---
+++ Assets/01.Scripts/State/IdleState.cs
@@ -0,0 +1,32 @@
+using NUnit.Framework.Interfaces;
+using UnityEngine;
+
+public class IdleState : Istate
+{
+    private StateMachine stateMachine;
+    private Animator animator;
+
+    public IdleState(StateMachine machine, Animator animator)
+    {
+        stateMachine = machine;
+        this.animator = animator;
+    }
+
+    public void Enter()
+    {
+        animator.Play("Idle");
+    }
+
+    public void Execute(Vector2 playerVector)
+    {
+        if (playerVector.magnitude > 0)
+        {
+            stateMachine.SetState(new RunState(stateMachine, animator));
+        }
+    }
+
+    public void Exit()
+    {
+        Debug.Log("Á¾·á");
+    }
+}
