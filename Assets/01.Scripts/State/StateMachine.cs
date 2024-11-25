20a2e6e5de19bc43df1e743d7fa47a06c71adfff
boguen <qhrms012@naver.com>
2024-11-25 20:54:07 +0900

    [add] 상태패턴

---
+++ Assets/01.Scripts/State/StateMachine.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class StateMachine 
+{
+    private Istate currentState;
+
+    public void SetState(Istate newState)
+    {
+        if(currentState != null)
+            currentState.Exit();
+
+        currentState = newState;
+        currentState.Enter();
+
+    }
+
+    public void Update(Vector2 PlayerVector)
+    {
+        if(currentState != null)
+            currentState.Execute(PlayerVector);
+    }
+}
