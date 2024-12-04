6c435554959bc718da3f706ca39fb449231ce646
boguen <qhrms012@naver.com>
2024-12-04 19:50:19 +0900

    [add] 오디오 믹서 매니저 및 컨트롤러 ui 설정

--- Assets/01.Scripts/Managers/AudioManager.cs
+++ Assets/01.Scripts/Managers/AudioManager.cs
@@ -1,16 +1,99 @@
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class AudioManager : Singleton<AudioManager>
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    [Header("BGM")]
+    public AudioClip bgmClip;
+    public float bgmVolume;
+    AudioSource bgmPlayer;
+    AudioHighPassFilter bgmHighPassFilter;
+    [SerializeField] private AudioMixerGroup bgmAudioMixerGroup;
+
+    [Header("SFX")]
+    public AudioClip[] sfxClip;
+    public float sfxVolume;
+    public int channels;
+    AudioSource[] sfxPlayers;
+    int channelIndex;
+    [SerializeField] private AudioMixerGroup sfxAudioMixerGroup;
+
+    public enum Sfx
     {
-        
+        Sensor,
+        Dead,
+        Lose,
+        Win
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        Init();
+    }
+    void Init()
+    {
+        // 배경음 플레이어 초기화
+        GameObject bgmObject = new GameObject("BgmPlayer");
+        bgmObject.transform.parent = transform;
+        bgmPlayer = bgmObject.AddComponent<AudioSource>();
+        bgmPlayer.playOnAwake = false;
+        bgmPlayer.loop = true;
+        bgmPlayer.volume = bgmVolume;
+        bgmPlayer.clip = bgmClip;
+        bgmHighPassFilter = Camera.main.GetComponent<AudioHighPassFilter>();
+        bgmPlayer.outputAudioMixerGroup = bgmAudioMixerGroup; // BGM 오디오 믹서 그룹 연결
+
+        //효과음 플레이어 초기화
+        GameObject sfxObject = new GameObject("SfxPlayer");
+        sfxObject.transform.parent = transform;
+        sfxPlayers = new AudioSource[channels];
+
+        for (int index = 0; index < sfxPlayers.Length; index++)
+        {
+            sfxPlayers[index] = sfxObject.AddComponent<AudioSource>();
+            sfxPlayers[index].playOnAwake = false;
+            sfxPlayers[index].bypassListenerEffects = true;
+            sfxPlayers[index].volume = sfxVolume;
+            sfxPlayers[index].outputAudioMixerGroup = sfxAudioMixerGroup; // SFX 오디오 믹서 그룹 연결
+        }
+
+    }
+
+    public void PlaySfx(Sfx sfx)
+    {
+
+        for (int index = 0; index < sfxPlayers.Length; index++)
+        {
+            int loopIndex = (index + channelIndex) % sfxPlayers.Length;
+
+            if (sfxPlayers[loopIndex].isPlaying)
+            {
+                continue;
+            }
+
+            int ranIndex = 0;
+
+            channelIndex = loopIndex;
+            sfxPlayers[loopIndex].clip = sfxClip[(int)sfx + ranIndex];
+            sfxPlayers[loopIndex].Play();
+            break;
+        }
+
+
+    }
+    public void SetBgmVolume(float volume)
+    {
+        bgmVolume = volume;
+        bgmPlayer.volume = bgmVolume;
+    }
+
+    public void SetSfxVolume(float volume)
+    {
+        sfxVolume = volume;
+        foreach (AudioSource sfxPlayer in sfxPlayers)
+        {
+            sfxPlayer.volume = sfxVolume;
+        }
     }
 }
